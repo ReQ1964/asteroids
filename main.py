b7849c8e26a22828b3aa0a1583b904a92a683573
ReQ1964 <slomianykacper4@gmail.com>
2025-06-03 19:53:09 +0200

    feat: slight refactor, add triple_shot base

--- main.py
+++ main.py
@@ -6,40 +6,55 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
 
-def main():
-    pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+def setup_groups_and_containers():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
+
     AsteroidField.containers = (updatable)
     Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
     Shot.containers = (updatable, drawable, shots)
+
+    return {
+        'updatable': updatable,
+        'drawable': drawable,
+        'asteroids': asteroids,
+        'shots': shots
+    }
+
+
+def main():
+    pygame.init()
+    groups = setup_groups_and_containers()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT/ 2)
     asteroidField = AsteroidField()
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
-        updatable.update(dt)
-        for asteroid in asteroids:
+        groups['updatable'].update(dt)
+        for asteroid in groups['asteroids']:
             if asteroid.collides(player):
                 print("Game over!")
                 sys.exit(0)
-            for shot in shots:
+            for shot in groups['shots']:
                 if asteroid.collides(shot):
                     asteroid.split()
                     shot.kill()
-        for item in drawable:
+        for item in groups['drawable']:
             item.draw(screen)
         pygame.display.flip()
         milliseconds = clock.tick(60)
         dt = milliseconds / 1000
 if __name__ == "__main__":
     main()
+
 
--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import pygame
 from constants import *
 from circleshape import CircleShape
 from shot import Shot
+from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -9,7 +10,7 @@ class Player(CircleShape):
         self.rotation = 0
         self.shot_cooldown = 0
         self.move_multiplier = 1
-        self.boost_duration = 2
+        self.boost_duration = PLAYER_BOOST_DURATION
         self.boost_cooldown = 0
         self.is_boosting = False
     # in the player class
@@ -33,8 +34,8 @@ class Player(CircleShape):
             if self.boost_duration <= 0:
                 self.is_boosting = False
                 self.move_multiplier = 1
-                self.boost_cooldown = 5
-                self.boost_duration = 2
+                self.boost_cooldown = PLAYER_BOOST_COOLDOWN
+                self.boost_duration = PLAYER_BOOST_DURATION
         else:
             self.boost_cooldown -= dt
 
@@ -47,7 +48,7 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            self.shoot_triple()
         if keys[pygame.K_LSHIFT]:
             self.boost()
     def move(self, dt):
@@ -59,6 +60,15 @@ class Player(CircleShape):
             shot = Shot(self.position, SHOT_RADIUS)
             shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
         self.shot_cooldown = PLAYER_SHOOT_COOLDOWN
+
+    def shoot_triple(self):
+        shot1 = Shot(self.position, SHOT_RADIUS)
+        shot1.velocity = pygame.Vector2(0,1).rotate(self.rotation + 15) * PLAYER_SHOOT_SPEED
+        shot2 = Shot(self.position, SHOT_RADIUS)
+        shot2.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        shot3 = Shot(self.position, SHOT_RADIUS)
+        shot3.velocity =pygame.Vector2(0,1).rotate(self.rotation - 15) * PLAYER_SHOOT_SPEED
+
 
     def boost(self):
         if self.boost_cooldown <= 0 and self.is_boosting is False:
